58782b1ac331014a659a44fcf5695897b366ee19
MartijnCurio <124584804+MartijnCurio@users.noreply.github.com>
2023-12-20 14:15:18 +0100

    Created Admin.cs

---
+++ CarDB/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDB
+{
+    internal class Admin
+    {
+        public static void ShowMatches()
+        {
+            // TODO: Show matches
+            Console.WriteLine("Show matches");
+        }
+
+        public static void ShowMatchResults()
+        {
+            // TODO: Show match results
+            Console.WriteLine("Show match results");
+        }
+    }
+}
